fd46c233493b880a9118f740395037cb6b5bd7e5
imforked <10011101.dev@gmail.com>
2025-09-14 17:27:26 -0700

    Add reCAPTCHA

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { SignUp } from "@imforked/legos";
 
 function App() {
-  return <SignUp action="http://localhost:4000" />;
+  return <SignUp action="http://localhost:4000" recaptchaSiteKey="6Lc0jMkrAAAAAP_-A8S6sUFh10RYmsMmBJgPvx-Z" />;
 }
 
 export default App;

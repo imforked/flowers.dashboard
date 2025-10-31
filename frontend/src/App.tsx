cbd4ce75d98a82b204ac15a3a7f135106d2ff1be
imforked <10011101.dev@gmail.com>
2025-10-30 18:10:55 -0700

    Remove comment.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,9 @@
-import { useEffect } from "react";
 import "./App.css";
 import { SignUp } from "@imforked/legos";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 function App() {
-  // TODO: remove this after you harvest ideas
-  // const currentPathname = window.location.pathname;
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const res = await fetch(`${BACKEND_URL}${currentPathname}`);
-  //       const data = await res.json();
-  //       console.log("Frontend received:", data);
-  //     } catch (err) {
-  //       console.error("Failed to fetch from backend:", err);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
-
   return (
     <SignUp
       action={`${BACKEND_URL}/signup`}

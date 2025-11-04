99364fea0c4487d79fa9951a9382d81a0194e375
imforked <10011101.dev@gmail.com>
2025-11-04 15:32:31 -0800

    Add /api/messages route.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,64 +1,39 @@
 import {
   Route,
   RequestMethod,
-  verifyRecaptcha,
-  ExpectedAction,
 } from "@imforked/legos/server";
-import { validateSignUp } from "./utils/validateSignUp.js";
-import bcrypt from "bcrypt";
 import { PrismaClient } from "./generated/prisma/index.js";
 
 const prisma = new PrismaClient();
 
 export const routes: Route[] = [
   {
-    path: "/signup",
+    path: "/api/messages",
     method: RequestMethod.POST,
-    requestHandler: async (req, res, next) => {
+
+    requestHandler: async (req, res) => {
       try {
-        const validationError = validateSignUp(req, res);
-        if (validationError) return;
-
-        // Verify reCAPTCHA
-        const { recaptchaToken, firstName, lastName, email, password } =
-          req.body;
-
-        console.log("Received signup request:", req.body);
-
-        const recaptchaOk = await verifyRecaptcha({
-          projectID: process.env.GCP_PROJECT_ID!,
-          recaptchaKey: process.env.RECAPTCHA_SITE_KEY!,
-          token: recaptchaToken,
-          expectedAction: ExpectedAction.SignUp,
-        });
-
-        if (!recaptchaOk) {
-          return res.status(400).json({ error: "Invalid reCAPTCHA token" });
+        const contentType = req.headers["content-type"];
+        if (!contentType?.startsWith("audio/wav")) {
+          return res
+            .status(400)
+            .json({ error: "Invalid content type. Expected audio/wav" });
         }
 
-        const saltRounds = 10;
-        const passwordHash = await bcrypt.hash(password, saltRounds);
-
-        const normalizedEmail = email.trim().toLowerCase();
-
-        const user = await prisma.user.create({
-          data: { firstName, lastName, email: normalizedEmail, passwordHash },
-        });
-
-        console.log("✅ New user created:", {
-          id: user.id,
-          email: user.email,
-          createdAt: user.createdAt,
-        });
-
-        // Remove hashed password from client
-        const { passwordHash: _, ...userSafe } = user;
-        res.json({ user: userSafe });
-      } catch (err: any) {
-        if (err.code === "P2002" && err.meta?.target?.includes("email")) {
-          return res.status(400).json({ error: "Email already in use" });
+        if (!req.body || !Buffer.isBuffer(req.body)) {
+          return res.status(400).json({ error: "No audio data received" });
         }
-        throw err;
+
+        const audioData = new Uint8Array(req.body);
+
+        const message = await prisma.message.create({
+          data: { audioData },
+        });
+
+        res.status(201).json({ id: message.id });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal server error" });
       }
     },
   },

8e316682dec4fd86675421210801172afeb506a8
imforked <10011101.dev@gmail.com>
2025-11-04 15:33:43 -0800

    Add middleware for audio/wav files.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,16 @@
-import "dotenv/config"
+import "dotenv/config";
 import { createServer } from "@imforked/legos/server";
 import { routes } from "./routes.js";
+import express from "express";
 
 // Create and configure the server
 const app = createServer({
-  // TODO: add CORS options
+  middleware: [
+    // JSON middleware for normal routes
+    express.json(),
+    // `.raw` middleware only for audio/wav requests
+    express.raw({ type: "audio/wav", limit: "10mb" }),
+  ],
   routes,
   onError: (err) => {
     console.error("Custom error handler:", err);
@@ -12,7 +18,6 @@ const app = createServer({
   },
 });
 
-// Start listening
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);

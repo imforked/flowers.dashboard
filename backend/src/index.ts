384ae93d9afb5c9a5514482c3b34ba3cdb2f694f
imforked <10011101.dev@gmail.com>
2025-11-08 10:00:59 -0800

    Add dynamic port value.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ const app = createServer({
   },
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });

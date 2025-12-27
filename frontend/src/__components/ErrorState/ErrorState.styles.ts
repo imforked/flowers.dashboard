312457fea6c26e9c86be2ec5e0aa011b0c6a1f5b
imforked <10011101.dev@gmail.com>
2025-12-27 09:38:06 -0800

    Improve error state UX.

--- frontend/src/__components/ErrorState/ErrorState.styles.ts
+++ frontend/src/__components/ErrorState/ErrorState.styles.ts
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px 0;
+`;
 
-export const Title = styled.h2``;
+export const Title = styled.h2`
+  margin: 0;
+`;
 
 export const Description = styled.span``;
--- frontend/src/__components/ErrorState/ErrorState.tsx
+++ frontend/src/__components/ErrorState/ErrorState.tsx
@@ -1,11 +1,13 @@
 import * as S from "./ErrorState.styles";
 import { type ErrorStateProps } from "./ErrorState.types";
+import { Button } from "@imforked/legos";
 
 export const ErrorState = ({ error }: ErrorStateProps) => {
   return (
     <S.Container>
       <S.Title>An Error Occurred</S.Title>
       <S.Description>{error}</S.Description>
+      <Button text="Try Again" onClick={() => window.location.reload()} />
     </S.Container>
   );
 };

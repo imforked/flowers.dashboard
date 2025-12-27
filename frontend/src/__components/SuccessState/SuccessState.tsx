85a57a629f14d751cb50f6a7dbcbc96fb609d4af
imforked <10011101.dev@gmail.com>
2025-12-27 09:32:32 -0800

    Add SuccessState

---
+++ frontend/src/__components/SuccessState/SuccessState.tsx
@@ -0,0 +1,9 @@
+import * as S from "./SuccessState.styles";
+
+export const SuccessState = () => {
+  return (
+    <S.Container>
+      <S.Title>Your message was submitted!</S.Title>
+    </S.Container>
+  );
+};
